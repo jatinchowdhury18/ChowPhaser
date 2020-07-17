3484141f4df4438fc39b3a6176a7f675d09fb4a0
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2020-07-16 23:13:36 -0700

    Update stereo plugin GUI

--- src/gui/TitleComp.h
+++ src/gui/TitleComp.h
@@ -3,14 +3,11 @@
 
 #include <JuceHeader.h>
 
-class TitleComp : public Component
+class TitleComp : public Component,
+                  public SettableTooltipClient
 {
 public:
-    TitleComp()
-    {
-        setColour (text1ColourID, Colours::white);
-        setColour (text2ColourID, Colours::grey);
-    }
+    TitleComp();
 
     enum ColourIDs
     {
@@ -18,26 +15,13 @@ public:
         text2ColourID,
     };
 
-    void paint (Graphics& g) override
-    {
-        g.setFont (Font ((float) getHeight()).boldened());
-        auto font = g.getCurrentFont();
-        auto b = getLocalBounds();
-
-        auto drawText = [=, &g, &b] (const String& text)
-        {
-            auto width = font.getStringWidth (text);
-            g.drawFittedText (text, b.removeFromLeft (width), Justification::left, 1);  
-        };
-
-        g.setColour (findColour (text1ColourID));
-        drawText (String ("CHOW Phaser "));
-
-        g.setColour (findColour (text2ColourID));
-        drawText (String ("(Mono)"));
-    }
+    void paint (Graphics& g) override;
+    void setStrings (String newTitle, String newSubtitle);
 
 private:
+    String title;
+    String subtitle;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(TitleComp)
 };
 
@@ -46,25 +30,15 @@ class TitleItem : public foleys::GuiItem
 public:
     FOLEYS_DECLARE_GUI_FACTORY (TitleItem)
 
-    TitleItem (foleys::MagicGUIBuilder& builder, const ValueTree& node) :
-        foleys::GuiItem (builder, node)
-    {
-        setColourTranslation ({
-            {"text1", TitleComp::text1ColourID},
-            {"text2", TitleComp::text2ColourID},
-        });
+    static const Identifier title;
+    static const Identifier subtitle;
 
-        addAndMakeVisible (comp);
-    }
+    TitleItem (foleys::MagicGUIBuilder& builder, const ValueTree& node);
 
-    void update() override
-    {
-    }
+    void update() override;
+    std::vector<foleys::SettableProperty> getSettableProperties() const override;
 
-    Component* getWrappedComponent() override
-    {
-        return &comp;
-    }
+    Component* getWrappedComponent() override { return &comp; }
 
 private:
     TitleComp comp;
