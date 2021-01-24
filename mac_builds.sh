a870924e258e64ac946ecda7895aa5b84af30d39
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2021-01-24 15:37:02 -0800

    Update build scripts (#8)
    * Start work on build scripts
    
    * Update build scripts
    
    Co-authored-by: jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>

---
+++ mac_builds.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# exit on failure
+set -e
+
+# clean up old builds
+rm -Rf build/
+rm -Rf bin/*Mac*
+
+# set up build VST
+VST_PATH=~/Developer/VST2_SDK/
+sed -i '' "7s~.*~juce_set_vst2_sdk_path(${VST_PATH})~" CMakeLists.txt
+sed -i '' '14s/#//' CMakeLists.txt
+sed -i '' '21s/#//' CMakeLists.txt
+
+# cmake new builds
+TEAM_ID=$(more ~/Developer/mac_id)
+cmake -Bbuild -GXcode -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY="Apple Distribution" \
+    -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$TEAM_ID \
+    -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_STYLE="Manual" \
+    -D"CMAKE_OSX_ARCHITECTURES=arm64;x86_64"
+cmake --build build --config Release -j8 | xcpretty
+
+# copy builds to bin
+mkdir -p bin/Mac
+declare -a plugins=("ChowPhaserMono" "ChowPhaserStereo")
+for plugin in "${plugins[@]}"; do
+    cp -R build/${plugin}_artefacts/Release/Standalone/${plugin}.app bin/Mac/${plugin}.app
+    cp -R build/${plugin}_artefacts/Release/VST/${plugin}.vst bin/Mac/${plugin}.vst
+    cp -R build/${plugin}_artefacts/Release/VST3/${plugin}.vst3 bin/Mac/${plugin}.vst3
+    cp -R build/${plugin}_artefacts/Release/AU/${plugin}.component bin/Mac/${plugin}.component
+done
+
+# reset CMakeLists.txt
+git restore CMakeLists.txt
+
+# zip builds
+VERSION=$(cut -f 2 -d '=' <<< "$(grep 'CMAKE_PROJECT_VERSION:STATIC' build/CMakeCache.txt)")
+(
+    cd bin
+    rm -f "ChowPhaser-Mac-${VERSION}.zip"
+    zip -r "ChowPhaser-Mac-${VERSION}.zip" Mac
+)
